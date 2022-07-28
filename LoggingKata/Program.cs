05ebb2502fd20c669a5494ac6b28b7b5bd592f6a
Joseph Broome <josephlloydbroome@gmail.com>
2022-07-28 16:40:51 -0700

    updates

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -15,105 +15,126 @@ namespace LoggingKata
 
         static void Main(string[] args)
         {
-
-
-            logger.LogInfo("Log initialized");
-
-
-            var lines = File.ReadAllLines(csvPath);
-
-            logger.LogInfo($"Lines: {lines[0]}");
-
-
-            var tacoparser = new TacoParser();
-
-
-            var locations = lines.Select(tacoparser.Parse).ToArray();
-
-
-            ITrackable tacoBellA = null;
-            ITrackable tacoBellB = null;
-            const double MetersToMiles = 0.000621371;
-
-            double distance = 0;
-            for (int i = 0; i < locations.Length; i++)
+            bool isnumber;
+            do
             {
-                var locationA = locations[i];
-                var coordinateA = new GeoCoordinate
+
+
+                Console.WriteLine("Welcome to the fast food visualizer app");
+                Console.WriteLine("Enter a fast food resturant and i will tell you which two locations are the farthest away from each other");
+                Console.Write("Please choose \n 1:For Tacobell \n 2:For BurgerKing \n");
+
+                var userchoice = Console.ReadLine();
+                int result;
+                isnumber = int.TryParse(userchoice, out result);
+                
+            if (result == 1)
                 {
-                    Latitude = locationA.Location.Latitude,
-                    Longitude = locationA.Location.Longitude,
-                };
-                for (int j = 0; j < locations.Length; j++)
-                {
-                    var locationB = locations[j];
-                    var coordinateB = new GeoCoordinate();
 
-                    coordinateB.Latitude = locationB.Location.Latitude;
-                    coordinateB.Longitude = locationB.Location.Longitude;
+                    logger.LogInfo("Log initialized");
 
 
-                    if (coordinateA.GetDistanceTo(coordinateB) > distance)
+                    var lines = File.ReadAllLines(csvPath);
+
+                    logger.LogInfo($"Lines: {lines[0]}");
+
+
+                    var tacoparser = new TacoParser();
+
+
+                    var locations = lines.Select(tacoparser.Parse).ToArray();
+
+
+                    ITrackable tacoBellA = null;
+                    ITrackable tacoBellB = null;
+                    const double MetersToMiles = 0.000621371;
+
+                    double distance = 0;
+                    for (int i = 0; i < locations.Length; i++)
                     {
-                        distance = coordinateA.GetDistanceTo(coordinateB);
-                        tacoBellA = locationA;
-                        tacoBellB = locationB;
+                        var locationA = locations[i];
+                        var coordinateA = new GeoCoordinate
+                        {
+                            Latitude = locationA.Location.Latitude,
+                            Longitude = locationA.Location.Longitude,
+                        };
+                        for (int j = 0; j < locations.Length; j++)
+                        {
+                            var locationB = locations[j];
+                            var coordinateB = new GeoCoordinate();
+
+                            coordinateB.Latitude = locationB.Location.Latitude;
+                            coordinateB.Longitude = locationB.Location.Longitude;
+
+
+                            if (coordinateA.GetDistanceTo(coordinateB) > distance)
+                            {
+                                distance = coordinateA.GetDistanceTo(coordinateB);
+                                tacoBellA = locationA;
+                                tacoBellB = locationB;
+                            }
+                        }
                     }
+                    logger.LogInfo($"{tacoBellA.Name} and {tacoBellB.Name} are the farthest apart from eachother and are {Convert.ToUInt32(distance) * MetersToMiles} miles apart");
+
                 }
-            }
-            logger.LogInfo($"{tacoBellA.Name} and {tacoBellB.Name} are the farthest apart from eachother and are {Convert.ToUInt32(distance) * MetersToMiles} miles apart");
-
-
-
-            logger2.LogInfo("Log initialized");
-
-
-            var line = File.ReadAllLines(csvPath2);
-
-            logger2.LogInfo($"Lines: {line[0]}");
-
-
-            var burgerkingparser = new BurgerKingParser();
-
-
-            var burgerKingLocations = line.Select(burgerkingparser.Parse).ToArray();
-            
-
-            ITrackable burgerKingA = null;
-            ITrackable burgerKingB = null;
-            const double MetersToMiles1 = 0.000621371;
-
-            double burgerKingDistance = 0;
-            for (int i = 0; i < burgerKingLocations.Length - 1; i++)
-            {
-                var burgerKinglocationA = burgerKingLocations[i];
-                var burgerKingcoordinateA = new GeoCoordinate
+                else if (result == 2)
                 {
-                    Latitude = burgerKinglocationA.Location.Latitude,
-                    Longitude = burgerKinglocationA.Location.Longitude,
-                };
-                for (int j = 0; j < burgerKingLocations.Length-1; j++)
-                {
-                    var burgerKinglocationB = burgerKingLocations[j];
-                    var burgerKingcoordinateB = new GeoCoordinate()
-                    {
-                        Latitude= burgerKinglocationB.Location.Latitude,
-                        Longitude= burgerKinglocationB.Location.Longitude,
-                    };
+                    logger2.LogInfo("Log initialized");
 
-                   
 
-                    if (burgerKingcoordinateA.GetDistanceTo(burgerKingcoordinateB) > burgerKingDistance)
+                    var line = File.ReadAllLines(csvPath2);
+
+                    logger2.LogInfo($"Lines: {line[0]}");
+
+
+                    var burgerkingparser = new BurgerKingParser();
+
+
+                    var burgerKingLocations = line.Select(burgerkingparser.Parse).ToArray();
+
+
+                    ITrackable burgerKingA = null;
+                    ITrackable burgerKingB = null;
+                    const double MetersToMiles1 = 0.000621371;
+
+                    double burgerKingDistance = 0;
+                    for (int i = 0; i < burgerKingLocations.Length - 1; i++)
                     {
-                        burgerKingDistance = burgerKingcoordinateA.GetDistanceTo(burgerKingcoordinateB);
-                        burgerKingA = burgerKinglocationA;
-                        burgerKingB = burgerKinglocationB;
+                        var burgerKinglocationA = burgerKingLocations[i];
+                        var burgerKingcoordinateA = new GeoCoordinate
+                        {
+                            Latitude = burgerKinglocationA.Location.Latitude,
+                            Longitude = burgerKinglocationA.Location.Longitude,
+                        };
+                        for (int j = 0; j < burgerKingLocations.Length - 1; j++)
+                        {
+                            var burgerKinglocationB = burgerKingLocations[j];
+                            var burgerKingcoordinateB = new GeoCoordinate()
+                            {
+                                Latitude = burgerKinglocationB.Location.Latitude,
+                                Longitude = burgerKinglocationB.Location.Longitude,
+                            };
+
+
+
+                            if (burgerKingcoordinateA.GetDistanceTo(burgerKingcoordinateB) > burgerKingDistance)
+                            {
+                                burgerKingDistance = burgerKingcoordinateA.GetDistanceTo(burgerKingcoordinateB);
+                                burgerKingA = burgerKinglocationA;
+                                burgerKingB = burgerKinglocationB;
+                            }
+                        }
                     }
+                    logger2.LogInfo($"{burgerKingA.Name} and {burgerKingB.Name} are the farthest apart from eachother and are {Convert.ToUInt32(burgerKingDistance) * MetersToMiles1} miles apart");
+
                 }
-            }
-            logger2.LogInfo($"{burgerKingA.Name} and {burgerKingB.Name} are the farthest apart from eachother and are {Convert.ToUInt32(burgerKingDistance) * MetersToMiles1} miles apart");
 
+                {
 
+                }
+            } while (isnumber == false);
         }
+
     }
 }
